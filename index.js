0c68afac799a74ded7e83a21151955967928fc7d
saidsoud72 <said.omar@student.moringaschool.com>
2022-08-23 09:41:26 +0300

    done deal

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, string){
+    const names = array.filter(name => name.toLowerCase() === string.toLowerCase());
+    return names;
+ }
+
+ function fuzzyMatch(array, string){
+     const driverNames = array.filter(name => name.charAt() === string.charAt());
+     return driverNames;
+ }
+
+ function matchName(driver, string){  
+     const match = driver.filter(details => details.name === string);
+     return match;
+ }
+ 
+ console.log(matchName(drivers, "Bobby"));
